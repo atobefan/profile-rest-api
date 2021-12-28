c55cd8b3b7f1f2ac88f9d2f891a2e713243d116f
Fan Zhang <fanzhang@berkeley.edu>
2021-12-27 20:27:24 -0800

    added helloApiview

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class HellowApiView(APIView):
+    """Test API Views"""
+
+    def get(self, request, format = None):
+        """Returns a list of APIView featurea"""
+        an_apiview = [
+        'Uses HTTP methods as function'
+        ]
+        return Response({'message':'Hello!','an_apiview':an_apiview})
